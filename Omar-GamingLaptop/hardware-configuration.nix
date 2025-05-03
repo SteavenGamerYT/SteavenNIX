18e280086a8c1ed31b5cd5fb7c7ca12ddae6b25e
Omar Hany Kasban <omarhanykasban706@gmail.com>
2025-05-03 15:21:02 +0300

    fixs

--- Omar-GamingLaptop/hardware-configuration.nix
+++ Omar-GamingLaptop/hardware-configuration.nix
@@ -108,6 +108,14 @@
         modesetting.enable = true;
         powerManagement.enable = true;
         powerManagement.finegrained = true;
+        prime = {
+            offload = {
+                enable = true;
+                enableOffloadCmd= true;
+            };
+            intelBusId = "PCI:0:2:0";
+            nvidiaBusId = "PCI:1:0:0";
+        };
     };
     sensor.hddtemp = {
       enable = true;
