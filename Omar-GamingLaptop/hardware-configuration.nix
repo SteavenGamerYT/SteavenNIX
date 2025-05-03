05f440332ac744e3729ac438fddde35cd7fb6df3
Omar Hany Kasban <omarhanykasban706@gmail.com>
2025-05-03 16:30:23 +0300

    fixs

--- Omar-GamingLaptop/hardware-configuration.nix
+++ Omar-GamingLaptop/hardware-configuration.nix
@@ -156,7 +156,7 @@
   # System services and packages
   services = {
     udev.extraRules = ''
-      SUBSYSTEM=="cpu", ACTION=="add", RUN+="/bin/bash -c 'if grep -q GenuineIntel /proc/cpuinfo; then chmod o+r /sys/class/powercap/intel-rapl:0/energy_uj; fi'"
+      SUBSYSTEM=="cpu", ACTION=="add", RUN+="${pkgs.bash}/bin/bash -c 'if grep -q GenuineIntel /proc/cpuinfo; then chmod o+r /sys/class/powercap/intel-rapl:0/energy_uj; fi'"
       SUBSYSTEM=="kvmfr", OWNER="omarhanykasban", GROUP="kvm", MODE="0660"
       SUBSYSTEM=="usb", ATTR{idVendor}=="040b", ATTR{idProduct}=="0897", ACTION=="add", RUN+="/bin/sh -c 'amixer -c headset set PCM 100% && amixer -c headset set PCM,1 100% && amixer -c Headset set PCM 100% && amixer -c Headset set PCM,1 100%'"
     '';
