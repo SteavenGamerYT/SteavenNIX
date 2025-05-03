2d2167ea8b978453cf6e26bc3f513f6949b1200a
Omar Hany Kasban <omarhanykasban706@gmail.com>
2025-05-03 19:07:06 +0300

    updates

--- Omar-PC-Server/configuration.nix
+++ Omar-PC-Server/configuration.nix
@@ -56,6 +56,21 @@ in
     extraGroups = [ "wheel" ];
   };
 
+  # Nix configuration
+  nixpkgs.config.allowUnfree = true;
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+      trusted-users = [ "root" username ];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
+
   # System packages
   environment.systemPackages = customPackages;
 
