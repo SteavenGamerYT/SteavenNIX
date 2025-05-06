9291d02f97b6fa5be1182ea743983e8eced68057
Omar Hany Kasban <omarhanykasban706@gmail.com>
2025-05-06 17:37:03 +0300

    updates

--- Omar-PC/hardware-configuration.nix
+++ Omar-PC/hardware-configuration.nix
@@ -38,7 +38,7 @@
     # Additional kernel parameters
     extraModprobeConfig = ''
       options amdgpu ppfeaturemask=0xFFFFFFFF
-      options vfio-pci ids=10de:2188,10de:1aeb,10de:1aec,10de:1aed
+      options vfio-pci ids=10de:2188,10de:1aeb,10de:1aec,10de:1aed,c0a9:540a
       options kvmfr static_size_mb=32
     '';
 
